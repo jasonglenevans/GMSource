57d4ec20c1cd0fe1227ce0a05d53c650d6e0c10f
jasonglenevans <56980043+jasonglenevans@users.noreply.github.com>
2022-04-27 15:37:49 -0400

    upload from script

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@ git add .
 git commit -m "upload from script"
 git branch -M main
 git remote add origin https://github.com/jasonglenevans/GMSource.git
-git push -u origin main
+git push -f origin main
---
+++ upload.sh.bak
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "upload from script"
+git branch -M main
+git remote add origin https://github.com/jasonglenevans/GMSource.git
+git push -u origin main
